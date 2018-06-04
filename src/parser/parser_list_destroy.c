6b29decc44a1e34bc21a382d3abb2fc403ccf3c6
Anthony <anthony.tram@epitech.eu>
2018-06-04 09:45:59 +0200

    [#5] - Parser destroy

---
+++ src/parser/parser_list_destroy.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2018
+** zappy
+** File description:
+** parser_destroyer
+*/
+
+#include <stdlib.h>
+#include "parser.h"
+
+void parser_destroy(void *game_info)
+{
+	parser_t *tmp = (parser_t *)game_info;
+	list_destroy(tmp->team_name);
+	free(game_info);
+}
