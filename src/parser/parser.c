a836eb9c62f35d36dbb37e3dec880b7340327333
Anthony <anthony.tram@epitech.eu>
2018-06-04 14:02:36 +0200

    [#5] - added errors handling

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -5,8 +5,10 @@
 ** parser
 */
 
+#include <stdio.h>
 #include <stdbool.h>
 #include "parser.h"
+#include "color.h"
 
 static bool parser_check(parser_t *game_info)
 {
@@ -14,17 +16,28 @@ static bool parser_check(parser_t *game_info)
 		&& (game_info->port) && list_get_size(game_info->team_name) &&
 		(game_info->width))
 		return (true);
-	else
+	else {
+		parser_destroy(game_info);
 		return (false);
+	}
 }
 
 parser_t *parser_create(int ac, char **av)
 {
-	parser_t *game_info = parser_alloc();
+	parser_t *game_info = NULL;
 
+	if (ac < 12) {
+		dprintf(2, "zappy_server: %sNot Enough Arguments\n",
+			ANSI_BOLD_COLOR_RED"error : "ANSI_COLOR_RESET);
+		return (NULL);
+	}
+	game_info = parser_alloc();
 	if (!game_info)
 		return (NULL);
-	parser_arg_handler(ac, av, game_info);
+	if (!parser_arg_handler(ac, av, game_info)) {
+		parser_destroy(game_info);
+		return (NULL);
+	}
 	if (parser_check(game_info))
 		return (game_info);
 	return (NULL);
