6eb91a0e7b6f33ff10ece0302a60ae6c49181188
Arthur C. Philippe <arthur.philippe@epitech.eu>
2018-06-04 09:38:53 +0200

    finished player_move_foward
    next up: tie player_move_foward to the msg cmd Forward

--- src/player/move.c
+++ src/player/move.c
@@ -11,10 +11,12 @@ void player_move_foward(player_t *pl, board_t *bd)
 {
 	pl->p_pos.v_x += pl->p_dir.v_x;
 	pl->p_pos.v_y += pl->p_dir.v_y;
-	pl->p_pos.v_x %= bd->b_max_x;
-	pl->p_pos.v_y %= bd->b_max_y;
 	if (pl->p_pos.v_x < 0)
-		pl->p_dir.v_x = bd->b_max_x - pl->p_dir.v_x;
+		pl->p_pos.v_x = bd->b_max_x + pl->p_dir.v_x;
+	else
+		pl->p_pos.v_x %= bd->b_max_x;
 	if (pl->p_pos.v_y < 0)
-		pl->p_dir.v_y = bd->b_max_x - pl->p_dir.v_y;
+		pl->p_pos.v_y = bd->b_max_y + pl->p_dir.v_y;
+	else
+		pl->p_pos.v_y %= bd->b_max_y;
 }
