0c1c70d48979d412223a72d3261e124b058a1cea
Anthony <anthony.tram@epitech.eu>
2018-06-12 13:45:28 +0200

    [#17] - added unit tests

--- tests/chrono.c
+++ tests/chrono.c
@@ -11,9 +11,41 @@
 #include "chrono.h"
 
 Test(Chrono, short_duration) {
-	chrono_t *ch = chrono_create(10);
+	chrono_t *ch = chrono_create(50);
 
-	cr_assert_eq(chrono_check(ch), false);
+	cr_assert_eq(chrono_check(ch), CHRONO_VALID);
+	cr_assert_eq(chrono_check(ch), CHRONO_VALID);
+	cr_assert_eq(chrono_check(ch), CHRONO_VALID);
+	cr_assert_eq(chrono_check(ch), CHRONO_VALID);
 	usleep(50000);
-	cr_assert_eq(chrono_check(ch), true);
+	cr_assert_eq(chrono_check(ch), CHRONO_EXPIRED);
 }
+
+Test(Chrono, short_duration_two) {
+	chrono_t *ch = chrono_create(5);
+
+	cr_assert_eq(chrono_check(ch), CHRONO_VALID);
+	cr_assert_eq(chrono_check(ch), CHRONO_VALID);
+	cr_assert_eq(chrono_check(ch), CHRONO_VALID);
+	cr_assert_eq(chrono_check(ch), CHRONO_VALID);
+	usleep(5000);
+	cr_assert_eq(chrono_check(ch), CHRONO_EXPIRED);
+}
+
+Test(Chrono, long_duration) {
+	chrono_t *ch = chrono_create(500);
+
+	cr_assert_eq(chrono_check(ch), CHRONO_VALID);
+	cr_assert_eq(chrono_check(ch), CHRONO_VALID);
+	cr_assert_eq(chrono_check(ch), CHRONO_VALID);
+	usleep(500000);
+	cr_assert_eq(chrono_check(ch), CHRONO_EXPIRED);
+}
+
+
+Test(Chrono, time_null) {
+	chrono_t *ch = chrono_create(0);
+
+	cr_assert_eq(ch, NULL);
+}
+
