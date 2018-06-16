4df326cf4d2dbbf0a0913d4014cdd34f975a0873
Arthur C. Philippe <arthur.philippe@epitech.eu>
2018-06-16 19:11:25 +0200

    [#40] eggs can now be used to added team slots

---
+++ server/src/game/on_cycle.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2018
+** server
+** File description:
+** on_cycle
+*/
+
+#include "game.h"
+#include "selector.h"
+
+void game_on_cycle(selector_t *stor)
+{
+	game_t *game = stor->s_data;
+
+}
