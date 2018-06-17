2fe2c840a96fb53aedc162f966936022a22daa59
Arthur C. Philippe <arthur.philippe@epitech.eu>
2018-06-17 14:24:21 +0200

    [#43] when player is supposed to die, a quit message is added to its queue.

--- server/src/player/lifespan_check.c
+++ server/src/player/lifespan_check.c
@@ -5,25 +5,26 @@
 ** lifespan_check
 */
 
+#include <string.h>
 #include <stdio.h>
 #include "player.h"
 #include "selector.h"
 
 void kill_player(selector_t *stor, handle_t *hdl, list_t *args)
 {
+	player_t *pl = hdl->h_data;
+
 	(void) args;
 	(void) stor;
 	dprintf(hdl->h_fd, "dead\n");
-	client_erase(stor, hdl);
+	list_push_back(pl->p_queued_msgs, strdup("quit"));
 }
 
 void player_lifespan_check(player_t *pl)
 {
 	if (chrono_check(&pl->p_lifespan) == CHRONO_EXPIRED &&
 		!pl->p_inventory[FOOD]) {
-		pl->p_task.dc_callback = kill_player;
-		pl->p_task.dc_args = NULL;
-		chrono_init(&pl->p_task.dc_timer, 0);
+		list_push_back(pl->p_queued_msgs, strdup("quit"));
 	} else if (chrono_check(&pl->p_lifespan) == CHRONO_EXPIRED) {
 		pl->p_inventory[FOOD] -= 1;
 		chrono_init(&pl->p_lifespan, LIFE_EXPECTANCY);
