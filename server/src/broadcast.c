3501fce5170b1305f4af72fed04af301402885eb
Arthur C. Philippe <arthur.philippe@epitech.eu>
2018-06-19 18:30:46 +0200

    codding style

--- server/src/broadcast.c
+++ server/src/broadcast.c
@@ -5,50 +5,49 @@
 ** broadcast
 */
 
-#include <stdlib.h>
 #include <math.h>
+#include <stdlib.h>
 #include "vector2d.h"
 
-float calc_prod_s(vector2d_t pt, vector2d_t pt2, vector2d_t height)
+static float calc_prod_s(vector2d_t pt, vector2d_t pt2, vector2d_t height)
 {
 	vector2d_t pt_pt2 = {pt2.v_x - pt.v_x, pt2.v_y - pt.v_y};
 	vector2d_t pt_height = {height.v_x - pt.v_x, height.v_y - pt.v_y};
-	float prod_s = (pt_pt2.v_x * pt_height.v_x)
-	+ (pt_pt2.v_y * pt_height.v_y);
+	float prod_s =
+		(pt_pt2.v_x * pt_height.v_x) + (pt_pt2.v_y * pt_height.v_y);
 
 	return (prod_s);
 }
 
-float calc_dist(vector2d_t pt, vector2d_t pt2, vector2d_t height)
+static float calc_dist(vector2d_t pt, vector2d_t pt2, vector2d_t height)
 {
-	float dist = sqrt(abs(pow(pt2.v_x - pt.v_x, 2))
-	+ abs(pow(pt2.v_y - pt.v_y, 2)));
-	float dist2 = sqrt(abs(pow(height.v_x - pt.v_x, 2))
-	+ abs(pow(height.v_y - pt.v_y, 2)));
+	float dist = sqrt(abs(pow(pt2.v_x - pt.v_x, 2)) +
+		abs(pow(pt2.v_y - pt.v_y, 2)));
+	float dist2 = sqrt(abs(pow(height.v_x - pt.v_x, 2)) +
+		abs(pow(height.v_y - pt.v_y, 2)));
 
 	return (dist * dist2);
 }
 
-int get_value(float angle)
+static int get_prior_tile_idx(float angle)
 {
-	switch ((int)angle)
-	{
-		case 0 ... 23:
-		case 338 ... 360:
+	switch ((int) angle) {
+	case 0 ... 23:
+	case 338 ... 360:
 		return (1);
-		case 24 ... 67:
+	case 24 ... 67:
 		return (2);
-		case 68 ... 112:
+	case 68 ... 112:
 		return (3);
-		case 113 ... 157:
+	case 113 ... 157:
 		return (4);
-		case 158 ... 202:
+	case 158 ... 202:
 		return (5);
-		case 203 ... 247:
+	case 203 ... 247:
 		return (6);
-		case 248 ... 292:
+	case 248 ... 292:
 		return (7);
-		case 293 ... 337:
+	case 293 ... 337:
 		return (8);
 	}
 	return (0);
@@ -64,7 +63,7 @@ int calc_broadcast(vector2d_t send, vector2d_t receive, vector2d_t dir)
 	printf("angle : %f \n", angle);
 	if (send.v_x - receive.v_x < 0)
 		angle = 360 - angle;
-	int nb = get_value(angle);
+	int nb = get_prior_tile_idx(angle);
 
 	return (nb);
 }
