f7ee616779461d6319723059db724f61a8d38b37
Victor <victor.deschamps@epitech.eu>
2018-06-20 16:10:38 +0200

    #59 - Now handling movments in a more generic way

--- player/include/FocusStrat.hpp
+++ player/include/FocusStrat.hpp
@@ -24,13 +24,13 @@ namespace pl {
 			bool isRuning() noexcept override {return _status;};
 		private:
 			void executeAction() noexcept;
-			void takeDecisions(int itemPos, unsigned long visionSize);
-			void moveToItem(int nbForward, int itemPos, int myPos);
-			int getClosestItem(std::vector<std::vector<std::string>> &vision);
+			void moveToItem(int itemPos);
+			int getClosestItemPos(std::vector<std::vector<std::string>> &vision);
 			void move(std::string direction) noexcept;
 			bool			_status;
 			Socket			&_socket;
 			std::queue<std::string>	_actionQueue;
+			std::string _itemName;
 	};
 }
 
--- player/src/FocusStrat.cpp
+++ player/src/FocusStrat.cpp
@@ -20,8 +20,8 @@ void pl::FocusStrat::run(std::vector<std::vector<std::string>> &vision) noexcept
 		executeAction();
 		return;
 	}
-	if ((itemPos = getClosestItem(vision)) > 0)
-		takeDecisions(itemPos, vision.size());
+	if ((itemPos = getClosestItemPos(vision)) > 0)
+		moveToItem(itemPos);
 	else
 		move("Right\n");
 }
@@ -43,44 +43,46 @@ void pl::FocusStrat::executeAction() noexcept
 		_status = false;
 }
 
-void pl::FocusStrat::takeDecisions(int itemPos, unsigned long visionSize)
-{
-	switch (visionSize) {
-	case 4:
-		moveToItem(1, itemPos, 2);
-	case 9:
-		moveToItem(2, itemPos, 6);
-	case 16:
-		moveToItem(3, itemPos, 12);
-	default:
-		break;
-	}
-}
-
-int pl::FocusStrat::getClosestItem(std::vector<std::vector<std::string>> &vision)
+int pl::FocusStrat::getClosestItemPos(std::vector<std::vector<std::string>> &vision)
 {
 	int itemPos = 0;
 
 	for (const auto &itemList : vision) {
 		for (auto item = itemList.begin(); item != itemList.end(); item++, itemPos++) {
-			if (!item->empty() && *item != "player")
+			if (!item->empty() && *item != "player") {
+				_itemName = *item;
 				return (itemPos);
+			}
 		}
 	}
 	return (0);
 }
 
-void pl::FocusStrat::moveToItem(int nbForward, int itemPos, int myPos)
+void pl::FocusStrat::moveToItem(int itemPos)
 {
-	while (nbForward-- > 0)
-		_actionQueue.push("Forward\n");
-	while (itemPos != myPos) {
-		if (itemPos < myPos) {
-			_actionQueue.push("Left\n");
-			myPos--;
-		} else if (itemPos > myPos) {
-			_actionQueue.push("Right\n");
-			myPos++;
+	int visionLevel = 1;
+	int middle = 0, min, max;
+	bool findLine = false;
+
+	while (findLine != true) {
+		middle = visionLevel * (visionLevel + 1);
+		min = middle - visionLevel;
+		max = middle + visionLevel;
+		if (min <= itemPos && itemPos <= max)
+			findLine = true;
+		else {
+			visionLevel++;
+			_actionQueue.push("Forward\n");
 		}
 	}
+	while (itemPos != middle) {
+		if (itemPos < middle) {
+			_actionQueue.push("Left\n");
+			middle--;
+		} else if (itemPos > middle) {
+			_actionQueue.push("Right\n");
+			middle++;
+		}
+	}
+	_actionQueue.push("Take " + _itemName + "\n");
 }
